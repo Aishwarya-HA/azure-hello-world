1707ad52a823e179c6760e86d9e4d8f124dea0a8
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-20 23:20:02 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,10 @@
 
 output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
+  description = "The public IP address of the VM."
+  value       = azurerm_public_ip.pip.ip_address
 }
 
 output "web_url" {
-  value = "http://${azurerm_public_ip.pip.ip_address}"
+  description = "The HTTP URL to access the deployed Hello World page."
+  value       = "http://${azurerm_public_ip.pip.ip_address}"
 }
