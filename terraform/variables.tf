51599c462666318fdbb253d9e7448f4e167cc661
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-20 16:54:16 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,36 @@
 
+// terraform/variables.tf
+
+// Your Azure subscription ID (passed from GitHub Secret: TF_VAR_SUBSCRIPTION_ID)
 variable "subscription_id" {
-  type = string
+  description = "Azure subscription ID where resources will be created."
+  type        = string
 }
 
+// Region to deploy resources into (we chose East US to avoid SKU capacity issues)
 variable "location" {
-  type    = string
-  default = "Central India"
+  description = "Azure region for all resources."
+  type        = string
+  default     = "East US"
 }
 
+// Prefix for resource names (becomes hello-rg, hello-vnet, etc.)
 variable "prefix" {
-  type    = string
-  default = "hello"
+  description = "Name prefix for all resources."
+  type        = string
+  default     = "hello"
 }
 
+// Admin username for the Linux VM
 variable "admin_username" {
-  type    = string
-  default = "azureuser"
+  description = "Admin username for the VM (SSH user)."
+  type        = string
+  default     = "azureuser"
 }
 
+// SSH public key used to access the VM (must be RSA: line starting with 'ssh-rsa')
 variable "ssh_public_key" {
-  type = string
+  description = "SSH PUBLIC key contents (e.g., output of: cat ~/.ssh/id_rsa.pub)."
+  type        = string
 }
+``
