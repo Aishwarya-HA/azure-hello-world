191b76b2d499fa1a7bc13a122fa0e71c14be5eb5
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-27 14:24:36 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -54,7 +54,7 @@ variable "ssh_public_key" {
 variable "vm_size" {
   description = "Size of the Azure Linux VM."
   type        = string
-  default     = "Standard_D2_v2"
+  default     =  "Standard_B1s"
 
 }
  
