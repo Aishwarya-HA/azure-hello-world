1e7680d46c96475f0e0c7579da488718834bac54
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-28 18:53:57 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,20 @@
 terraform {
   required_version = ">= 1.6.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.111"
+      # Pin to a stable version to avoid recent regressions.
+      version = "= 3.103.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.10"
     }
   }
 }
 
 provider "azurerm" {
-  features       {}
+  features {}
   subscription_id = var.subscription_id
 }
