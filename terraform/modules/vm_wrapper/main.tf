5007641706ab0ac35f6b402fd44c3965dbd68494
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-28 18:42:33 +0530

    Update main.tf

--- terraform/modules/vm_wrapper/main.tf
+++ terraform/modules/vm_wrapper/main.tf
@@ -1,7 +1,18 @@
 #############################################
+# Workaround: control-plane propagation delay
+# (prevents "Root object was present, but now absent" on NIC)
+#############################################
+resource "time_sleep" "network_delay" {
+  # give Azure a moment after VNet/Subnet/PIP are created in root
+  create_duration = "8s"
+}
+
+#############################################
 # NIC
 #############################################
 resource "azurerm_network_interface" "nic" {
+  depends_on = [time_sleep.network_delay]
+
   name                = "${var.name}-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -14,12 +25,21 @@ resource "azurerm_network_interface" "nic" {
   }
 
   tags = var.tags
+
+  # Optional: ignore short-lived diffs while Azure populates fields
+  lifecycle {
+    ignore_changes = [
+      ip_configuration[0].private_ip_address
+    ]
+  }
 }
 
 #############################################
 # Linux VM
 #############################################
 resource "azurerm_linux_virtual_machine" "vm" {
+  depends_on = [azurerm_network_interface.nic]
+
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -62,4 +82,14 @@ resource "azurerm_linux_virtual_machine" "vm" {
       admin_ssh_key
     ]
   }
+
+  # Sometimes creation is slow due to control-plane delays
+  timeouts {
+    create = "30m"
+    update = "30m"
+  }
 }
+
+#############################################
+# (Module outputs are in outputs.tf)
+#############################################
