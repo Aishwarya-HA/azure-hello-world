0ec533f1bc83882aa273535ec1843e48d3653504
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-28 16:07:59 +0530

    Update variables.tf

--- terraform/modules/vm_wrapper/variables.tf
+++ terraform/modules/vm_wrapper/variables.tf
@@ -1,12 +1,39 @@
-variable "name"                { description = "Base name for VM and resources"; type = string }
-variable "location"            { description = "Azure region"; type = string }
-variable "resource_group_name" { description = "Resource group name"; type = string }
+variable "name" {
+  description = "Base name for VM and resources"
+  type        = string
+}
 
-variable "subnet_id"    { description = "Subnet ID"; type = string }
-variable "public_ip_id" { description = "Public IP ID (optional)"; type = string, default = null }
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
 
-variable "admin_username" { type = string }
-variable "admin_ssh_key"  { type = string, sensitive = true }
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+}
+
+variable "public_ip_id" {
+  description = "Public IP ID (optional)"
+  type        = string
+  default     = null
+}
+
+variable "admin_username" {
+  description = "Admin username for SSH"
+  type        = string
+}
+
+variable "admin_ssh_key" {
+  description = "SSH public key for the admin user"
+  type        = string
+  sensitive   = true
+}
 
 variable "vm_size" {
   description = "VM size (e.g., Standard_B1s, Standard_B2s)"
@@ -15,7 +42,7 @@ variable "vm_size" {
 }
 
 variable "source_image_reference" {
-  description = "Image for the VM"
+  description = "Image to use for the VM"
   type = object({
     publisher = string
     offer     = string
@@ -31,12 +58,13 @@ variable "source_image_reference" {
 }
 
 variable "custom_data_b64" {
-  description = "Base64-encoded cloud-init user data"
+  description = "Base64-encoded cloud-init user data (optional)"
   type        = string
   default     = null
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
 }
