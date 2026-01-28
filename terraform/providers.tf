ceb37ee95031034986e2c267a1d4cfe8bc05dee5
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-28 17:40:55 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,18 +6,9 @@ terraform {
       version = "~> 3.111"
     }
   }
-
-  # Optional: remote state backend (configure if you already use it)
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-terraform-state"
-  #   storage_account_name = "mystatetfstorage"
-  #   container_name       = "tfstate"
-  #   key                  = "hello-world-app.tfstate"
-  #   use_azuread_auth     = true
-  # }
 }
 
 provider "azurerm" {
-  features {}
+  features       {}
   subscription_id = var.subscription_id
 }
