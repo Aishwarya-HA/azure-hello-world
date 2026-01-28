0293bd9c8e9c9c0c976b69f8705b08f043bec1d4
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-28 16:13:44 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,3 @@
-################################
-# Subscription / Region
-################################
 variable "subscription_id" {
   description = "Azure subscription ID where resources will be deployed."
   type        = string
@@ -12,40 +9,26 @@ variable "location" {
   default     = "eastus"
 }
 
-################################
-# Naming
-################################
 variable "prefix" {
   description = "Prefix used for naming Azure resources."
   type        = string
   default     = "helloweb"
-  validation {
-    condition     = can(regex("^[a-z0-9-]{3,24}$", var.prefix))
-    error_message = "prefix must be 3-24 chars with lowercase letters, digits or hyphens."
-  }
 }
 
-################################
-# Admin access
-################################
 variable "admin_username" {
-  description = "Admin username for SSH access to the VM."
+  description = "Admin username for SSH access."
   type        = string
   default     = "azureuser"
 }
 
 variable "admin_ssh_key" {
-  description = "SSH public key for authentication (contents of ~/.ssh/id_rsa.pub)."
+  description = "SSH public key for the admin user."
   type        = string
   sensitive   = true
 }
 
-################################
-# VM size (resize knob)
-################################
 variable "vm_size" {
-  description = "Size of the Azure Linux VM."
+  description = "VM size (e.g., Standard_B1s, Standard_B2s). Change to trigger wrapper resize."
   type        = string
   default     = "Standard_B1s"
 }
-``
