06cbda7957e91f55cec028cd94f0df55b8215efd
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-01-28 16:03:47 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,35 +1,30 @@
-
 terraform {
   required_version = ">= 1.6.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.116.0"
+      version = "~> 3.111"
     }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.11"
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
     }
   }
 
-  # Remote state in Azure Storage (uses OIDC/Azure AD auth via your GitHub Action)
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state-aish"
-    storage_account_name = "aishterraformstate"
-    container_name       = "tfstate"
-    key                  = "hello-world.tfstate"
-    use_azuread_auth     = true
-  }
+  # Optional: Remote state backend (recommended)
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-terraform-state"
+  #   storage_account_name = "mystatetfstorage"
+  #   container_name       = "tfstate"
+  #   key                  = "hello-world-app.tfstate"
+  #   use_azuread_auth     = true
+  # }
 }
 
-# AzureRM provider
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
-  # If you ever see provider registration delays, you can optionally add:
-  # skip_provider_registration = true
 }
 
-# time provider (needed for time_sleep resources)
-provider "time" {}
+# Needed for data "template_file" rendering cloud-init
+provider "template" {}
