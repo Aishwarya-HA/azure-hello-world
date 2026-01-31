2c47364630ead40622e85e31e31fc9b32dc14ba9
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-02-01 00:44:31 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,49 +1,20 @@
-##############################################
-# Root outputs.tf (UPDATED)
-##############################################
-
-# -------------------------------------------
-# Infrastructure / workflow helper outputs
-# -------------------------------------------
-
-output "resource_group_name" {
-  description = "Resource Group used by this deployment."
-  value       = azurerm_resource_group.rg.name
-}
-
-output "location" {
-  description = "Azure region where resources are deployed."
-  value       = azurerm_resource_group.rg.location
-}
-
-# -------------------------------------------
-# VM information (from wrapper module)
-# -------------------------------------------
-
-# IMPORTANT:
-# Your module block is named `web_vm`
-# If your module name is different, update `module.web_vm` accordingly
-
-output "vm_name" {
-  description = "Name of the Linux VM created by the wrapper module."
-  value       = module.web_vm.vm_name
-}
-
-output "private_ip" {
-  description = "Private IP address of the VM NIC."
-  value       = module.web_vm.private_ip
-}
-
-# -------------------------------------------
-# Public access
-# -------------------------------------------
-
 output "public_ip" {
-  description = "Public IP address of the VM."
+  description = "The public IP address of the VM."
   value       = azurerm_public_ip.pip.ip_address
 }
 
 output "web_url" {
-  description = "HTTP URL to access the VM (if a web service is running)."
+  description = "The HTTP URL to access the deployed Hello World page."
   value       = "http://${azurerm_public_ip.pip.ip_address}"
+}
+
+# Optional: helpful for debugging/visibility
+output "vm_name" {
+  description = "VM name created by the wrapper"
+  value       = module.web_vm.vm_name
+}
+
+output "private_ip" {
+  description = "Private IP address of the VM NIC"
+  value       = module.web_vm.private_ip
 }
