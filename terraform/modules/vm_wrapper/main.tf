90e9d351a9b78e8fbcf24b1dc684dc105d26f07f
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-02-02 13:42:00 +0530

    Update main.tf

--- terraform/modules/vm_wrapper/main.tf
+++ terraform/modules/vm_wrapper/main.tf
@@ -53,9 +53,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   # Required when password auth is disabled
   admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.admin_ssh_key
-  }
+  username   = var.admin_username
+  public_key = var.ssh_public_key
+}
 
   os_disk {
     name                 = "${var.name}-osdisk"
