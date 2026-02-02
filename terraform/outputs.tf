d7fb2eed51eb9ebcb43a0b7578490920e0285052
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-02-02 16:01:54 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,6 @@
 # Root outputs.tf (safe references)
 #############################################
 
-# Resource group and subnet always exist in root
 output "resource_group_name" {
   description = "Resource group created by root."
   value       = azurerm_resource_group.rg.name
@@ -13,28 +12,29 @@ output "subnet_id" {
   value       = azurerm_subnet.subnet.id
 }
 
-# --- From VM module (use try(...) so validate doesn't fail if output is absent) ---
-
-# Public IP of VM (assumes module exposes `public_ip`)
-# If your module uses a different name (e.g., `public_ip_address`), change below accordingly.
-output "public_ip" {
-  description = "The public IP address of the VM (from the module)."
-  value       = try(module.web_vm.public_ip, null)
-}
-
-# Convenience URL (only if public_ip exists)
-output "web_url" {
-  description = "The HTTP URL to access the deployed Hello World page."
-  value       = try("http://${module.web_vm.public_ip}", null)
-}
-
-# Helpful visibility outputs (these already reference the module in your file)
+# From VM module (the module currently exposes vm_name, nic_id, private_ip)
 output "vm_name" {
   description = "VM name created by the wrapper module."
   value       = try(module.web_vm.vm_name, null)
 }
 
+output "nic_id" {
+  description = "NIC ID created by the wrapper module."
+  value       = try(module.web_vm.nic_id, null)
+}
+
 output "private_ip" {
   description = "Private IP address of the VM NIC (from the module)."
   value       = try(module.web_vm.private_ip, null)
+}
+
+# If your module later exposes public_ip, these will automatically work.
+output "public_ip" {
+  description = "The public IP address of the VM (from the module)."
+  value       = try(module.web_vm.public_ip, null)
+}
+
+output "web_url" {
+  description = "The HTTP URL to access the deployed Hello World page."
+  value       = try("http://${module.web_vm.public_ip}", null)
 }
